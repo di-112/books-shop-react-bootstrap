82dc1f95105f75a72ac54719495024930e05e6a4
di-112 <73027626+di-112@users.noreply.github.com>
2021-01-11 20:00:34 +0300

    v2.0

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <HashRouter >
     <Provider store={store} >
       <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
